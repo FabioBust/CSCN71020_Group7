9b07f13c6c4bdc5da0474f136118cb1cf0e19421
Fabio Bustamante Romillo <f.bustamanteromillo@gmail.com>
2025-11-12 15:57:56 -0500

    Added function to calculare internal angles of the triangle.

--- PolygonChecker/triangleSolver.c
+++ PolygonChecker/triangleSolver.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 #include "triangleSolver.h"
 
@@ -21,4 +22,15 @@ char* analyzeTriangle(int side1, int side2, int side3) {
 	}
 
 	return result;
+}
+
+void angleCalculator(float side1, float side2, float side3) {
+	float angle1 = acos((side2 * side2 + side3 * side3 - side1 * side1) / 2 * side2 * side3);
+	float angle2 = acos((side1 * side1 + side3 * side3 - side2 * side2) / 2 * side1 * side3);
+	float angle3 = 180 - angle1 - angle2;
+
+	printf("The inside angles in the triangle are:\n");
+	printf("Angle 1: %lf", angle1);
+	printf("Angle 2: %lf", angle2);
+	printf("Angle 3: %lf", angle3);
 }
--- PolygonChecker/triangleSolver.h
+++ PolygonChecker/triangleSolver.h
@@ -1,2 +1,3 @@
 #pragma once
 char* analyzeTriangle(int side1, int side2, int side3);
+void angleCalculator(float side1, float side2, float side3);
