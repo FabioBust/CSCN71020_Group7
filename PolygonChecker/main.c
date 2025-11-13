96730f6d501814ad03847e84b9ee1c9d019f8a68
C-pennings <charliepennings13@gmail.com>
2025-11-12 23:27:23 -0500

    Fixed a header file

--- PolygonChecker/main.c
+++ PolygonChecker/main.c
@@ -56,7 +56,7 @@ float* getTriangleSides(float* triangleSides) {
 	printf_s("Enter the three sides of the triangle: ");
 	for (int i = 0; i < 3; i++)
 	{
-		scanf_s("%lf", &triangleSides[i]);
+		scanf_s("%f", &triangleSides[i]);
 	}
 	return triangleSides;
 }
