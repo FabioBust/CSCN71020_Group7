354373cd163ec567734aa779f3e2aa3e1b621f3e
C-pennings <charliepennings13@gmail.com>
2025-11-26 15:05:34 -0500

    fixed the rectangle checker to work for unorder points

--- PolygonChecker/rectangleSolver.h
+++ PolygonChecker/rectangleSolver.h
@@ -2,4 +2,5 @@
 float Perimeter(float points[]);
 float Area(float points[]);
 void pointSorter(float points[], float out[]);
-bool is_rectangle(float p1[], float p2[], float p3[], float p4[]);
+bool isRectangle(float p1[2], float p2[2], float p3[2], float p4[2]);
+float distanceSquared(float p1[2], float p2[2]);
